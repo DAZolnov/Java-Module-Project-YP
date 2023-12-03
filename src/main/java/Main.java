5a0d72ddb24675ea5df8ed90d787cf1a58881471
DAZolnov <w-im-ff@yandex.ru>
2023-12-03 21:22:08 +0300

    Расчет стоимости товаров

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,36 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Locale.setDefault(Locale.US);
+        System.out.println("Привет, на какое количество людей поделим счет?\nПожалуйста, введите целое число больше 1");
+        Scanner scanner = new Scanner(System.in);
+        int amountOfPeople;
+        while(true) {
+            if (scanner.hasNextInt()) {
+                amountOfPeople = scanner.nextInt();
+                if (amountOfPeople > 1) {
+                    System.out.println("Количество людей, на которых поделим счет: " + amountOfPeople);
+                    break;
+                } else {
+                    System.out.println("Вы ввели некорректное значение.\nПожалуйста, будьте внимательны и введите целое число больше 1");
+                }
+            } else {
+                scanner.nextLine();
+                System.out.println("Неверный ввод данных. Пожалуйста, введите ЦЕЛОЕ число больше 1");
+            }
+        }
+
+
+
+        Product product = new Product("", 0);
+        double totalPrice = product.printListOfProducts();
+
+        Formatter formatter = new Formatter(totalPrice, amountOfPeople);
+        formatter.printFinalResult();
+        scanner.close();
     }
 }
+
---
+++ src/main/java/Product.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Product {
+    String nameOfProduct;
+    double priceOfProduct;
+
+
+
+
+    Product(String nameOfProduct,double priceOfProduct) {
+        this.nameOfProduct = nameOfProduct;
+        this.priceOfProduct = priceOfProduct;
+    }
+    Scanner scannerProduct = new Scanner(System.in);
+    public double printListOfProducts() {
+
+        while(true) {
+            System.out.println("Введите название нового товара. Если хотите прекратить, введите \"завершить\"");
+            String oneMoreProduct = scannerProduct.next();
+                if (oneMoreProduct.equalsIgnoreCase("завершить")) {
+                    break;
+                } else {
+                    if (!(oneMoreProduct.trim().isEmpty())) {
+                        nameOfProduct += oneMoreProduct + "\n";
+                        priceOfProduct += calcPrice (oneMoreProduct);
+                        scannerProduct.nextLine();
+                    } else {
+                        System.out.println("Введите корректное наименование товара: пробелы не считаются!");
+                        scannerProduct.nextLine();
+                    }
+                }
+        }
+        System.out.println("Добавленные товары:\n" + nameOfProduct);
+        scannerProduct.close();
+        return priceOfProduct;
+        }
+
+    public double calcPrice (String nameOfProduct) {
+        //double oneMorePrice = 0;
+        System.out.println("Введите стоимость товара \"" + nameOfProduct + "\" в формате рубли.копейки");
+        while(true) {
+            if (scannerProduct.hasNextDouble()) {
+                double oneMorePrice = scannerProduct.nextDouble();
+                if (oneMorePrice > 0) {
+                    return oneMorePrice;
+                } else {
+                    System.out.println("Вы ввели некорректное значение.\nПожалуйста, будьте внимательны и введите число больше 0");
+                }
+            } else {
+
+                System.out.println("Неверный ввод данных. Пожалуйста, введите ЧИСЛОВОЙ эквивалент стоимости");
+                scannerProduct.nextLine();
+            }
+        }
+    }
+}
